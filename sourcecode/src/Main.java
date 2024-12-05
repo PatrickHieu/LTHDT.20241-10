aa8aabd07c0670608d743ccf19d315aee0ebf1cc
Patrick_Hieu <hieunguyenthanh204@gmail.com>
2024-12-05 23:14:20 +0700

    Complete feature: Analyze Circuit

--- sourcecode/src/Main.java
+++ sourcecode/src/Main.java
@@ -1,4 +1,19 @@
+import circuit.Circuit;
+import elements.Capacitor;
+import elements.Inductor;
+import elements.Resistor;
+import simulator.CircuitAnalyzer;
+
 public class Main {
     public static void main(String[] args) {
+        Circuit circuit = new Circuit("Serial");
+
+        //Add elements
+        circuit.addElement(new Resistor("R1", 10));
+        circuit.addElement(new Capacitor("C2", 0.0001));
+        circuit.addElement(new Inductor("L3", 0.01));
+
+        //Analyze circuit
+        CircuitAnalyzer.analyzeSerial(circuit, 10);
     }
 }
