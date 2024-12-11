d9a5a7c213c6c393e08d96977c76ee23222f57a0
NguyenTrongHinh <127172728+NguyenTrongHinh@users.noreply.github.com>
2024-12-11 13:46:29 +0700

    Capacitor.java

--- sourcecode/src/elements/Capacitor.java
+++ sourcecode/src/elements/Capacitor.java
@@ -8,15 +8,19 @@ public class Capacitor extends CircuitElement {
 
     @Override
     public double calculateVoltage(double current) {
-        // Tính trở kháng tụ điện: V = I * (1 / 2πfC)
         double frequency = 50; 
         return current * (-1 / (2 * Math.PI * frequency * getValue()));
     }
 
     @Override
     public double calculateCurrent(double voltage) {
-        // Tính I = V * (2πfC)
         double frequency = 50;
         return voltage * (2 * Math.PI * frequency * getValue());
     }
+    @Override
+    public String toString() {
+        return "Capacitor{name='" + getName() + "', capacitance=" + getValue() + "}";
+    }
+
+
 }
