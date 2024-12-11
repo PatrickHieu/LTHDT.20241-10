449a775314e38e7d51306597bd29a3510898c558
NguyenTrongHinh <127172728+NguyenTrongHinh@users.noreply.github.com>
2024-12-11 13:47:01 +0700

    Inductor.java

--- sourcecode/src/elements/Inductor.java
+++ sourcecode/src/elements/Inductor.java
@@ -8,15 +8,19 @@ public class Inductor extends CircuitElement {
 
     @Override
     public double calculateVoltage(double current) {
-        // tính trở kháng cuộn cảm: V = I * (2πfL)
         double frequency = 50; 
         return current * (2 * Math.PI * frequency * getValue());
     }
 
     @Override
     public double calculateCurrent(double voltage) {
-       // tính I = V / (2πfL)
         double frequency = 50;
         return voltage / (2 * Math.PI * frequency * getValue());
     }
+    @Override
+    public String toString() {
+        return "Inductor{name='" + getName() + "', inductance=" + getValue() + "}";
+    }
+
+
 }
